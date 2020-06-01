92a9f695ed50320ab53dc325cd8500111befb599
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-01 11:57:14 -0700

    add functions to calculator
    co-authored by: Mark Tiapkov <mark.tiapkov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,21 @@ const add = function(number1, number2) {
     return number1 + number2;
 };
 
+const subtract = function(number1, number2){
+    return number1-number2;
+}
+
+const multiply = function(number1, number2){
+    return number1*number2;
+}
+
+const divide = function(number1, number2){
+    return number1/number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const result = "Addition: " + add(number1, number2) + ", Subtraction: " 
++ subtract(number1, number2) + ", Multiplication: " + multiply(number1, number2)
++ ", Division: " + divide(number1, number2);
 alert(result);

816eaaf48cc341e8222ca366410f7ef7feaf8330
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-03 16:25:08 -0700

    add jQuery functionality for computation
    co-authored by: Johnny Duverseau <duveerseaujohnny21@gmai.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,33 @@ const divide = function(number1, number2){
     return number1/number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = "Addition: " + add(number1, number2) + ", Subtraction: " 
-+ subtract(number1, number2) + ", Multiplication: " + multiply(number1, number2)
-+ ", Division: " + divide(number1, number2);
-alert(result);
+$(function() {
+  $("form#add").submit(function(event){
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = add(number1, number2);
+      $("#add-output").text(result);
+  });
+  $("form#sub").submit(function(event){
+      event.preventDefault();
+      const number1 = parseInt($("#sub1").val());
+      const number2 = parseInt($("#sub2").val());
+      const result = subtract(number1, number2);
+      $("#sub-output").text(result);
+  });
+  $("form#mult").submit(function(event){
+      event.preventDefault();
+      const number1 = parseInt($("#mult1").val());
+      const number2 = parseInt($("#mult2").val());
+      const result = multiply(number1, number2);
+      $("#mult-output").text(result);
+  });
+  $("form#div").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#div-output").text(result);
+  });
+});

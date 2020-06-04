e0fc6dde90f26e167eb22883a9e63f2cf6008026
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-04 15:16:58 -0700

    remove forms from index.html and add one radio button form, change scripts.js file
    co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,10 @@
-const add = function(number1, number2) {
-    return number1 + number2;
-};
-
-const subtract = function(number1, number2){
-    return number1-number2;
-}
-
-const multiply = function(number1, number2){
-    return number1*number2;
-}
-
-const divide = function(number1, number2){
-    return number1/number2;
-}
-
-$(function() {
-  $("form#add").submit(function(event){
+$(document).ready(function() {
+    $("form#calculator").submit(function() {
       event.preventDefault();
-      const number1 = parseInt($("#add1").val());
-      const number2 = parseInt($("#add2").val());
+      const number1 = parseInt($("#input1").val());
+      const number2 = parseInt($("#input2").val());
+      const operator = $("input:radio[name=operator]:checked").val();
       const result = add(number1, number2);
-      $("#add-output").text(result);
+      $("#output").text(result);
+    });
   });
-  $("form#sub").submit(function(event){
-      event.preventDefault();
-      const number1 = parseInt($("#sub1").val());
-      const number2 = parseInt($("#sub2").val());
-      const result = subtract(number1, number2);
-      $("#sub-output").text(result);
-  });
-  $("form#mult").submit(function(event){
-      event.preventDefault();
-      const number1 = parseInt($("#mult1").val());
-      const number2 = parseInt($("#mult2").val());
-      const result = multiply(number1, number2);
-      $("#mult-output").text(result);
-  });
-  $("form#div").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#div1").val());
-    const number2 = parseInt($("#div2").val());
-    const result = divide(number1, number2);
-    $("#div-output").text(result);
-  });
-});

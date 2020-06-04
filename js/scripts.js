d2299d0c7ee40a3159b0113235029f939aad05ea
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-04 15:56:18 -0700

    add branching to check user input and add styles for output
    co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,17 +19,27 @@ $(document).ready(function() {
       event.preventDefault();
       const number1 = parseInt($("#input1").val());
       const number2 = parseInt($("#input2").val());
+      if(!number1 || !number2)
+      {
+          alert("Incorrect input, not a number");
+          return;
+      }
+      let symbol;
       const operator = $("input:radio[name=operator]:checked").val();
       let result;
       if (operator === "add") {
         result = add(number1, number2);
+        symbol = "+";
       } else if (operator === "subtract") {
         result = subtract(number1, number2);
+        symbol = "-";
       } else if (operator === "multiply") {
         result = multiply(number1, number2);
+        symbol = "*";
       } else if (operator === "divide") {
         result = divide(number1, number2);
+        symbol = "/";
       }
-      $("#output").text(result);
+      $("#output").text(number1 + " " + symbol + " " + number2 + " = " + result);
     });
   });
